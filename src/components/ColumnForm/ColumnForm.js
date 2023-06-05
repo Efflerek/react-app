bada63b9f70cdbcd3e50047b77d5ae902a353103
Efflerek <piotr.effler@gmail.com>
2023-06-05 22:41:08 +0100

    Updated Card in Card.js CardForm, Column, ColumnForm, List

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,31 +1,29 @@
 import styles from './ColumnForm.module.scss';
-import Button from '../Button/Button';
-import TextInput from '../TextInput/TextInput';
+/*import { useEffect } from 'react';*/
 import { useState } from 'react';
-
+import Button from '../Button/Button.js';
+import TextInput from '../TextInput/TextInput.js';
 const ColumnForm = props => {
-    const [title, setTitle] = useState('');
-    const [icon, setIcon] = useState(''); 
-
-    const handleSubmit = e => {
+  const [title, setTitle] = useState('');
+  const [icon, setIcon] = useState('');
+  const handleSubmit = e => {
       e.preventDefault();
       props.action({ title: title, icon: icon });
       setTitle('');
       setIcon('');
-};
-
-	return (
+  }
+  return (
     <form onSubmit={handleSubmit} className={styles.columnForm}>
-<div className={styles.columnInputs}>
+      <div className={styles.columnInputs}>
         <span className={styles.inputTitle}>Title:</span> <TextInput value={title} onChange={e => setTitle(e.target.value)} />
       </div>
       <div className={styles.columnInputs}>
         <span className={styles.inputTitle}>Icon:</span> <TextInput value={icon} onChange={e => setIcon(e.target.value)} />
       </div>
-<Button>Add column</Button>
-</form>
-
-	);
+        <Button>
+              Add column
+        </Button>
+    </form>
+  );
 };
-
 export default ColumnForm;
