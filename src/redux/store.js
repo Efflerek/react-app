2866fe050a240de63cdd188ee9a7e5c700718b72
Efflerek <piotr.effler@gmail.com>
2023-06-08 20:18:26 +0100

    Redux - updated index.js , added store.js

---
+++ src/redux/store.js
@@ -0,0 +1,24 @@
+import { createStore, combineReducers } from 'redux';
+import initialState from './initialState';
+import shortid from "shortid";
+import listsReducer from './listsReducer';
+import columnsReducer from './columnsReducer';
+import cardsReducer from './cardsReducer';
+import searchStringReducer from './searchStringReducer';
+
+const subreducers = {
+  lists: listsReducer,
+  columns: columnsReducer,
+  cards: cardsReducer,
+  searchString: searchStringReducer
+}
+
+const reducer = combineReducers(subreducers);
+
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+export default store;
