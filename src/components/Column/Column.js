1191e89b299a217ceee1c84006846fe0a5ce5c50
Efflerek <piotr.effler@gmail.com>
2023-06-08 20:55:43 +0100

    Updated. Column.js

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,13 +1,16 @@
 import styles from './Column.module.scss'
 import Card from '../Card/Card.js';
 import CardForm from '../CardForm/CardForm.js';
+import { useSelector } from 'react-redux';
 
 const Column = props => {
+  const cards = useSelector(state => state.cards.filter(card => card.columnId === props.id));
+
   return (
     <article className={styles.column}>
       <h3 className={styles.title}><span className={styles.icon + ' fa fa-' + props.icon} />{props.title}</h3>
       <ul className={styles.cards}>
-        {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+        {cards.map(card => <Card key={card.id} title={card.title} />)}
       </ul>
       <CardForm columnId={props.id} action={props.action} />
     </article>
@@ -16,13 +19,3 @@ const Column = props => {
 
 export default Column;
 
-//ALTERNATYWA
-//const Column = ({ children }) => {
-   // return (
-//<article className={styles.column}>
-//<h2 className={styles.title}>{children}</h2>
-//</article>
-//);
-//};
-
-//export default Column;
