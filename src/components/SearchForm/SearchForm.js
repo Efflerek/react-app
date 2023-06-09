f177c88c1cff17d8ef807ab5f54c9f543979d9cc
Efflerek <piotr.effler@gmail.com>
2023-06-09 19:19:31 +0100

    Add notfound , favorite, about, home, pagetitle

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,21 +1,16 @@
-import styles from './SearchForm.module.scss'
+import styles from './SearchForm.module.scss';
 import TextInput from '../TextInput/TextInput.js';
 import Button from '../Button/Button.js';
 import { useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { updateSearchString } from '../../redux/store';
-
 const SearchForm = () => {
-
     const [searchString, setSearch] = useState('');
-
     const handleSubmit = e => {
         e.preventDefault();
         dispatch(updateSearchString({ searchString }));
     };
-
     const dispatch = useDispatch();
-
   return (
       <form onSubmit={handleSubmit} className={styles.searchForm}>
           <TextInput placeholder="Search..." value={searchString} onChange={e => setSearch(e.target.value)}/>
@@ -25,5 +20,4 @@ const SearchForm = () => {
       </form>
   );
 };
-
 export default SearchForm;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Provider } from 'react-redux';
@@ -6,12 +7,15 @@ import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.querySelector('#root')
 );
